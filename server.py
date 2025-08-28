62765f7c62c4869dad4ec945993287b4b73b0008
rthaller <rene.thaller@campus02.at>
2025-08-28 23:05:03 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -37,6 +37,4 @@ def fragebogen_speichern():
     return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
